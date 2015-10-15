ed4cbce5ecbe262ef5e0e5d7e081d36a67f0fe20
dockiller2003 <docdockiller@aol.com>
2015-10-15 10:31:05 -0500

    Add files
    Add files

---
+++ app-services/user.service.js
@@ -0,0 +1,29 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .factory('UserService', UserService);
+
+    UserService.$inject = ['$http'];
+    function UserService($http) {
+        var service = {};
+        service.Create = Create;
+        return service;
+
+        function Create(user) {
+            return $http.post('/api/users', user).then(handleSuccess, handleError('Error creating user'));
+        }
+
+        function handleSuccess(res) {
+            return res.data;
+        }
+
+        function handleError(error) {
+            return function () {
+                return { success: false, message: error };
+            };
+        }
+    }
+
+})();
